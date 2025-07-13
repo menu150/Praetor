e379e62348b8be8c36ec32075bf9a60c7c5211e4
Nate Leffler <nathanleffler@gmail.com>
2025-07-12 23:34:52 -0500

    Real-time update: 2025-07-12 23:34:52

---
+++ pages/intel-feed.js
@@ -0,0 +1,10 @@
+import IntelFeedPanel from "@/components/ui_intel_feed.jsx";
+
+export default function IntelFeedPage() {
+  return (
+    <main className="min-h-screen bg-gray-100 p-8">
+      <IntelFeedPanel />
+    </main>
+  );
+}
+
