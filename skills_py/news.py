e42130b98ee7e277c8925dd2977f5b77491c543d
Nate Leffler <nathanleffler@gmail.com>
2025-07-26 00:20:18 -0500

    Real-time update: 2025-07-26 00:20:18

--- skills_py/news.py
+++ skills_py/news.py
@@ -1,4 +1,6 @@
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import requests
 
 from praetor.nlp.summarizer import summarize_article  # Update if your function name is different
