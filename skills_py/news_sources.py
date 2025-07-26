74f280f3ed571b3fb043ac0725b1c26299493a92
Nate Leffler <nathanleffler@gmail.com>
2025-07-25 22:53:34 -0500

    Real-time update: 2025-07-25 22:53:34

--- skills_py/news_sources.py
+++ skills_py/news_sources.py
@@ -10,3 +10,11 @@ def fetch_global_sources():
     resp = requests.get(SOURCES_URL, params={"apiKey": API_KEY})
     data = resp.json()
     return [s["id"] for s in data.get("sources", []) if s.get("id")]
+
+CURATED_SOURCES = [
+    "reuters", "bbc-news", "al-jazeera-english", "the-verge", "bloomberg",
+    "axios", "techcrunch", "financial-times", "politico"
+]
+
+def get_curated_sources():
+    return CURATED_SOURCES
